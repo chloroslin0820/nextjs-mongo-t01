d1c47ea1f47073ce8b34ba00ec925aff1e9c0ded
chloros <chloros.lin@gmail.com>
2024-06-21 15:18:03 +0800

    complete login/register panel UI

---
+++ app/(auth)/layout.js
@@ -0,0 +1,16 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Auth Halo Chat",
+  description: "Build a Next 14 Chat App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-purple-1`}>{children}</body>
+    </html>
+  );
+}

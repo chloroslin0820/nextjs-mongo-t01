0022ad2d1ffbee5f61a8ef6765b676a5d147a993
chloros <chloros.lin@gmail.com>
2024-06-21 16:19:33 +0800

    complete dataschema setting, db config, postAPI

---
+++ app/api/auth/register/route.js
@@ -0,0 +1,37 @@
+import User from "@models/User";
+import { connectToDB } from "@mongodb";
+import { hash } from "bcryptjs";
+
+export const POST = async (req, res) => {
+    try {
+        await connectToDB();
+        const { username, email, password } = await req.json();
+
+        const existingUser = await User.findOne({ email });
+        if(existingUser) {
+            return new Response("User already exists", {
+                status: 400,
+            });
+        }
+
+        const hashedPassword = await hash(password, 10);
+
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+        });
+
+        await newUser.save();
+
+        return new Response(JSON.stringify(newuser), {
+            status: 200,
+        });
+
+    } catch (error) {
+        console.log(error);
+        return new Response("Failed to create a new user", {
+            status: 500,
+        });
+    }
+}
